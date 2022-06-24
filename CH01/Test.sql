7a1e77ccae5216c1dc2135b817e014dbb942d9b8
unknown <digh5420@naver.com>
2022-06-24 14:22:51 +0900

    20220624

---
+++ CH01/Test.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `user2`(
+	`uid` CHAR(5),
+	`name` VARCHAR(10),
+	`hp` CHAR(20),
+	`age` int
+);
+
+INSERT INTO `user2` VALUES ('a101', '홍길동', '010-1212-1011', 21);
+INSERT INTO `user2` VALUES ('a102', '김춘추', '010-1234-2222', 23);
+INSERT INTO `user2` VALUES ('a103', '장보고', '010-1234-3333', 35);
+INSERT INTO `user2` VALUES ('a104', '강감찬', '010-1234-4444', 45);
+INSERT INTO `user2` VALUES ('a105', '이순신', '010-1234-5555', 51);
+INSERT INTO `user2` VALUES ('a106', '정약용', '010-1234-3333', 42);
+INSERT INTO `user2` VALUES ('a107', '임꺽정', '010-1212-1010', 38);
